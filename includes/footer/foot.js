9d955eb87afc4d0724b95a160c4b5536ca4941a4
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-30 21:04:58 +0100

    Delete includes/footer/foot.js

--- includes/footer/foot.js
+++
@@ -1,7 +0,0 @@
-fetch("/zweefvliegen/includes/footer/foot.html")
-  .then((response) => response.text())
-  .then((html) => {
-    html = html.replace("{brand}", brand);
-    
-    document.getElementById("footer-placeholder").innerHTML = html;
-  });
