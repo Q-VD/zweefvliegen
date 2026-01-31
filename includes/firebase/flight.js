3baf6eb2e9c46d91209ef01489b503df0f7cecb3
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 23:32:48 +0100

    Create flight.js

---
+++ includes/firebase/flight.js
@@ -0,0 +1,31 @@
+import { db } from "./firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+//haal docId uit query string
+const params = new URLSearchParams(window.location.search);
+const docId = params.get("docId");
+
+if (docId) {
+  const docRef = doc(db, "Logbook", docId);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    document.getElementById("flightNumber").textContent = data.flightNumber;
+    document.getElementById("Date").textContent = data.Date;
+    document.getElementById("StartTime").textContent = data.StartTime;
+    document.getElementById("LandingTime").textContent = data.LandingTime;
+    document.getElementById("Duration").textContent = data.Duration;
+    document.getElementById("StartLocation").textContent = data.StartLocation;
+    document.getElementById("LandingLocation").textContent = data.LandingLocation;
+    document.getElementById("FrontSeat").textContent = data.FrontSeat;
+    document.getElementById("BackSeat").textContent = data.BackSeat;
+    document.getElementById("Remark").textContent = data.Remarl;
+  }
+  else {
+    document.getElementById("flightNumber").textContent = "Vlucht niet gevonden!";
+  }
+}
+else {
+  document.getElementById("flightNumber").textContent = "Geen vlucht geselecteerd!";
+}
