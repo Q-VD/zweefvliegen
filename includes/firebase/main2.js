42795c5f54de12c8246546f0337e6eb981d1c089
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 20:23:16 +0100

    Update main2.js

--- includes/firebase/main2.js
+++ includes/firebase/main2.js
@@ -2,17 +2,22 @@ import { db } from "./firebase.js";
 import {
   collection,
   addDoc,
+  setDoc,
   getDocs
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 let x = 0;
 
 document.getElementById("add").addEventListener("click", async () => {
-  await addDoc(collection(db, "Notes"), {
+  const docId = "Note" + (++x);
+
+  const docRef = doc(db, "Notes", docId);
+  
+  await setDoc(docRef, {
     Title: "Test title",
-    message: ++x,
+    message: "Dit is bericht" + x,
     created: Date.now()
   });
 
-  console.log("Notitie toegevoegd");
+  console.log("Notitie toegevoegd  met ID", docId);
 });
