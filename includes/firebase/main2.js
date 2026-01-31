9c3df0330e52d38743be9b2d36a2f40a386c3a40
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 22:38:19 +0100

    Update main2.js

--- includes/firebase/main2.js
+++ includes/firebase/main2.js
@@ -1,13 +1,6 @@
 import { db } from "./firebase.js";
 import {collection, setDoc, getDocs, doc} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Functie om document ID te maken: timestamp + titel
-//function generateDocId(title) {
-  //const timestamp = Date.now();
-  //const safeTitle = title.replace(/\s+/g, "_").toLowerCase(); //Spaties vervangen
-  //return `${safeTitle}_${timestamp}`;
-//}
-
 //Voeg notitie toe
 document.getElementById("add").addEventListener("click", async () => {
   const title = document.getElementById("title").value.trim();
