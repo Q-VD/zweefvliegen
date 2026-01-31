284301a19ff486bd7256fe81007e59aa93d0645d
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 21:21:57 +0100

    Update note.js

--- includes/firebase/note.js
+++ includes/firebase/note.js
@@ -1,1 +1,20 @@
+import { db } from "./firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+//haal docId uit query string
+const params = nes URLSearchParams(window.location.search);
+const docId = params.get("docId");
+
+if (docId) {
+  const data = docSnap.data();
+  document.getElementById("noteTitle").textContent = data.Title;
+  document.getElementById("noteMessage").textContent = data.message;
+  const date = new Date(date.created);
+  document.getElementById("noteDate").textContent = `Aangemaakt op: ${date.toLocaleString()}`;
+  }
+else {
+  document.getElementById("noteTitle").textContent = "Notitie niet gevonden";
+}
+else {
+  document.getElementById("noteTitle").textContent = "Geen notitie geselecteerd";
+}
