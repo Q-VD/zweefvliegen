66a57132edd192dc17dd5be96fa3003b38ba6ebd
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 20:03:46 +0100

    Create main.js

---
+++ includes/firebase/main.js
@@ -0,0 +1,15 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+document.getElementById("add").addEventListener("click", async () => {
+  await addDoc(collection(db, "test"), {
+    message: "Hallo GitHub Pages",
+    created: Date.now()
+  });
+
+  console.log("Document toegevoegd");
+});
