bbfb866ef99ab0bf3ec79229622cbf6a34f63a6b
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 21:34:05 +0100

    Update note.js

--- includes/firebase/note.js
+++ includes/firebase/note.js
@@ -1,20 +1,23 @@
 import { db } from "./firebase.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-//haal docId uit query string
+// haal docId uit query string
 const params = new URLSearchParams(window.location.search);
 const docId = params.get("docId");
 
 if (docId) {
-  const data = docSnap.data();
-  document.getElementById("noteTitle").textContent = data.Title;
-  document.getElementById("noteMessage").textContent = data.message;
-  const date = new Date(date.created);
-  document.getElementById("noteDate").textContent = `Aangemaakt op: ${date.toLocaleString()}`;
+  const docRef = doc(db, "Notes", docId);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    document.getElementById("noteTitle").textContent = data.Title;
+    document.getElementById("noteMessage").textContent = data.message;
+    const date = new Date(data.created);
+    document.getElementById("noteDate").textContent = `Aangemaakt op: ${date.toLocaleString()}`;
+  } else {
+    document.getElementById("noteTitle").textContent = "Notitie niet gevonden";
   }
-else {
-  document.getElementById("noteTitle").textContent = "Notitie niet gevonden";
-}
-else {
+} else {
   document.getElementById("noteTitle").textContent = "Geen notitie geselecteerd";
 }
