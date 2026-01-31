49574b17478512c5795855490906704db91fea5e
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-01-31 20:43:39 +0100

    Update main2.js

--- includes/firebase/main2.js
+++ includes/firebase/main2.js
@@ -1,24 +1,36 @@
 import { db } from "./firebase.js";
 import {
   collection,
-  addDoc,
   setDoc,
   getDocs,
   doc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-let x = 0;
+// Functie om document ID te maken: timestamp + titel
+function generateDocId(title) {
+  const timestamp = Date.now();
+  const safeTitle = title.replace(/\s+/g, "_").toLowerCase(); //Spaties vervangen
+  return `${safeTitle}_${timestamp}`;
+}
 
 document.getElementById("add").addEventListener("click", async () => {
-  const docId = "Note " + (++x);
+  const title = document.getElementById("title").value.trim();
+  const message = document.getElementById("message").value.trim();
 
+  if (!title || !message) {
+  alert("Vul zowel title als bericht in!");
+  return;
+  }
+
+  const docId = generateDocId(title);
   const docRef = doc(db, "Notes", docId);
-  
+
   await setDoc(docRef, {
-    Title: "Test title",
-    message: "Dit is bericht" + x,
+    Title: title,
+    message: message,
     created: Date.now()
   });
 
-  console.log("Notitie toegevoegd  met ID ", docId);
+  console.log("Notitie toegevoegd met ID: ", docID);
+
 });
