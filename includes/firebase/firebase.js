e5b99be247e54c68bce8c2e6a77c6bebe7df08c7
Quinten Van Dommelen <qvandommelen55@gmail.com>
2026-02-01 14:16:42 +0100

    Update firebase.js

--- includes/firebase/firebase.js
+++ includes/firebase/firebase.js
@@ -1,12 +1,18 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxf7yVlaa3vWvweKZ3acsK0o_GpGUCuVE",
   authDomain: "zweefvliegen-45079.firebaseapp.com",
   projectId: "zweefvliegen-45079",
+  appId: "1:833199799502:web:4b6009d9ee21a818b1649f"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialiseer app één keer
+export const app = initializeApp(firebaseConfig);
+
+// Exporteer services
+export const auth = getAuth(app);
 export const db = getFirestore(app);
